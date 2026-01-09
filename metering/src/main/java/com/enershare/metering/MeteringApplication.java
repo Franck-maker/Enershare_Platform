ff66df273f91a3ead5c4757ad27e3f7886945ef4
Franck Abdiel <foziefranck@gmail.com>
2026-01-09 09:21:17 +0100

    project initialization

---
+++ metering/src/main/java/com/enershare/metering/MeteringApplication.java
@@ -0,0 +1,11 @@
+package com.enershare.metering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MeteringApplication.class, args);
+    }
+}
